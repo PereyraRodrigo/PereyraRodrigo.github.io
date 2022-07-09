9f5c5d10c7515e4f90645d90158d348cbf0a1f77
Rodrigo Pereyra <pereyrarodrigo@hotmail.com>
2022-07-09 02:37:10 -0300

    added footer,navbar. WIP skills

---
+++ js/shownavbar.js
@@ -0,0 +1,92 @@
+(function(){
+
+    var doc = document.documentElement;
+    var w   = window;
+
+    /*
+    define four variables: curScroll, prevScroll, curDirection, prevDirection
+    */
+
+    var curScroll;
+   //    var prevScroll = w.scrollY || doc.scrollTop;
+  var prevScroll = window.pageYOffset;
+    var curDirection = 0;
+    var prevDirection = 0;
+
+    /*
+    how it works:
+    -------------
+    create a scroll event listener
+    create function to check scroll position on each scroll event,
+    compare curScroll and prevScroll values to find the scroll direction
+    scroll up - 1, scroll down - 2, initial - 0
+    then set the direction value to curDirection
+    compare curDirection and prevDirection
+    if it is different, call a function to show or hide the header
+    example:
+    step 1: user scrolls down: curDirection 2, prevDirection 0 > hide header
+    step 2: user scrolls down again: curDirection 2, prevDirection 2 > already hidden, do nothing
+    step 3: user scrolls up: curDirection 1, prevDirection 2 > show header
+    */
+
+    var header = document.getElementById('site-header');
+    var toggled;
+    var threshold = 200;
+
+    var checkScroll = function() {
+               // curScroll = w.scrollY || doc.scrollTop;
+      curScroll = window.pageYOffset;
+        if(curScroll > prevScroll) {
+            // scrolled down
+            curDirection = 2;
+        }
+        else {
+            //scrolled up
+            curDirection = 1;
+        }
+
+        if(curDirection !== prevDirection) {
+            toggled = toggleHeader();
+        }
+
+        prevScroll = curScroll;
+        if(toggled) {
+            prevDirection = curDirection;
+        }
+    };
+
+    var toggleHeader = function() { 
+        toggled = true;
+        if(curDirection === 2 && curScroll > threshold) {
+            header.classList.add('hide');
+        }
+        else if (curDirection === 1) {
+            header.classList.remove('hide');
+        }
+        else {
+            toggled = false;
+        }
+        return toggled;
+    };
+//initialize throttleTimer as false
+let throttleTimer = false;
+ 
+const throttle = (callback, time) => {
+    //don't run the function while throttle timer is true
+    if (throttleTimer) return;
+     
+    //first set throttle timer to true so the function doesn't run
+    throttleTimer = true;
+     
+    setTimeout(() => {
+        //call the callback function in the setTimeout and set the throttle timer to false after the indicated time has passed 
+        callback();
+        throttleTimer = false;
+    }, time);
+}
+
+window.addEventListener('scroll', () => {
+  throttle(checkScroll, 250);
+})
+})();
+
